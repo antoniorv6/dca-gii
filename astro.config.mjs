dc5275dd55ed59636bbc5db9a00babe06534d2c0
Antonio Ríos Vila <36246417+antoniorv6@users.noreply.github.com>
2025-09-15 13:19:54 +0200

    Updated broken theory links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   base:'/dca-gii/',   //TODO descomentar para build
-  trailingSlash: 'always',  //TODO descomentar para build
+  //trailingSlash: 'always',  //TODO descomentar para build
   site:'https://antoniorv6.github.io/',
   build:{
     assets: '_assets'
